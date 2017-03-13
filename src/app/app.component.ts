42fae73dd93364e85268fe3d873da38d757ff15b
Piotr <piotr.kasperek@gmail.com>
2017-03-13 21:13:27 +0100

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { Http, Response } from '@angular/http';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+
+    private backendUrl = 'http://localhost:8080';
+    constructor(private http: Http) {
+
+    }
+
+    testBackend() {
+        this.http.get(this.backendUrl + '/photo/test').subscribe((response) => console.log(response));
+    }
+
 }
