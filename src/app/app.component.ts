cddec59ad3962a1ac4bb815d9b9ad1509ceab152
Piotr <piotr.kasperek@gmail.com>
2017-03-26 20:13:03 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Http, Response } from '@angular/http';
 @Component({
     selector: 'app-root',
@@ -7,13 +8,37 @@ import { Http, Response } from '@angular/http';
 })
 export class AppComponent {
 
+    @ViewChild('file') FileInput: ElementRef;
+    filePath = '';
+
     private backendUrl = 'http://localhost:8080';
-    constructor(private http: Http) {
+    constructor(private http: Http, private sanitizer: DomSanitizer) {
 
     }
 
     testBackend() {
-        this.http.get(this.backendUrl + '/photo/test').subscribe((response) => console.log(response));
+        const file = this.FileInput.nativeElement.files[0];
+        this.filePath = `./../../assets/bmp/${file.name}`;
+        // this.http.get(this.backendUrl + '/photo/test')
+        //     .subscribe((response) => console.log(response));
+    }
+
+    sendFile() {
+        const file = this.FileInput.nativeElement.files[0];
+        const formData: FormData = new FormData();
+        formData.append('file', file, file.name);
+        console.log(file);
+        console.log(formData);
+        this.http.post(this.backendUrl + '/photo/upload', formData)
+            .subscribe(
+            (response) => this.filePath = `./../../assets/bmp/${file.name}`,
+            (error) => console.log(error)
+            );
+    }
+
+    filter() {
+        this.http.get(this.backendUrl + '/photo/filter')
+            .subscribe((response) => console.log(response));
     }
 
 }
