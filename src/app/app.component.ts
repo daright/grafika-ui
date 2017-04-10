1bd02b2844963597c7442038fbaf68f32a4216c4
Piotr <piotr.kasperek@gmail.com>
2017-04-10 20:32:14 +0200

    bla bla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,53 +2,60 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Http, Response } from '@angular/http';
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-    @ViewChild('file') FileInput: ElementRef;
-    filePath = '';
-    photoVisible = false;
-    private filename = '';
+  @ViewChild('file') FileInput: ElementRef;
+  filePath = '';
+  photoVisible = false;
+  private filename = '';
 
-    private backendUrl = 'http://localhost:8080';
-    constructor(private http: Http, private sanitizer: DomSanitizer) {
+  private backendUrl = 'http://localhost:8080';
+  constructor(private http: Http, private sanitizer: DomSanitizer) {
 
-    }
+  }
 
-    testBackend() {
-        const file = this.FileInput.nativeElement.files[0];
-        this.filename = file.name;
+  testBackend() {
+    const file = this.FileInput.nativeElement.files[0];
+    this.filename = file.name;
+    this.filePath = `./../../assets/bmp/${file.name}`;
+    this.http.get(this.backendUrl + '/photo/test')
+      .subscribe((response) => console.log(response));
+  }
+
+  sendFile() {
+    const file = this.FileInput.nativeElement.files[0];
+    const formData: FormData = new FormData();
+    formData.append('file', file, file.name);
+    this.filename = file.name;
+    this.http.post(this.backendUrl + '/photo/upload', formData)
+      .subscribe(
+      (response) => {
+        console.log(response);
         this.filePath = `./../../assets/bmp/${file.name}`;
-        this.http.get(this.backendUrl + '/photo/test')
-            .subscribe((response) => console.log(response));
-    }
+        // this.photoLoaded();
+      },
+      (error) => console.log(error),
+      () => this.photoLoaded()
+      );
+  }
 
-    sendFile() {
-        const file = this.FileInput.nativeElement.files[0];
-        const formData: FormData = new FormData();
-        formData.append('file', file, file.name);
-        this.filename = file.name;
-        this.http.post(this.backendUrl + '/photo/upload', formData)
-            .subscribe(
-            (response) => {
-                this.filePath = `./../../assets/bmp/${file.name}`;
-                this.photoVisible = true;
-            },
-            (error) => console.log(error)
-            );
-    }
+  photoLoaded() {
+    this.photoVisible = true;
+  }
 
-    filter() {
-        this.photoVisible = false;
-        const timeStamp = new Date().getTime();
-        this.http.get(this.backendUrl + '/photo/filter')
-            .subscribe((response) => {
-                this.photoVisible = true;
-                this.filePath = `./../../assets/bmp/${this.filename}?time=${timeStamp}`;
-            });
-    }
+  filter() {
+    this.photoVisible = false;
+    const timeStamp = new Date().getTime();
+    this.http.get(this.backendUrl + '/photo/filter')
+      .subscribe((response) => {
+        console.log(response);
+        this.filePath = `./../../assets/bmp/${this.filename}?time=${timeStamp}`;
+        this.photoLoaded();
+      });
+  }
 
 }
